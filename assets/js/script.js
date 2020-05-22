4e45054cac143be308b06dfdc972a209b053f18d
hex008080 <louisebjer@gmail.com>
2020-05-22 13:26:24 +0200

    summodal
    heh

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,3 +82,29 @@ $(document).ready(function () {
 function doNothing(e) {
     e.stopPropagation();
 }
+//hej modal
+// Get the modal
+var modal = document.getElementById("deleteModal");
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("delete-resource");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// klik på knap, åben modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// klik på <span> (cancel), lukker modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
